d4e05c3836ce64818e21afefeb73cb355c0ba3aa
David Churchill <davinian@mac.com>
2018-05-21 16:38:37 +0100

    org file
    first push

---
+++ definables.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * 
+ * List of definable options for wp-config file
+ * Author: David Churchill
+ * Updated: 2018-05-21
+ * 
+ **/
+
+
+/** API for Awsome Weather Widget Pro 
+ * URL: https://halgatewood.com
+ **/
+define('AWESOME_WEATHER_PRO_LICENSE', 'awsome_weather_pro_license');
+define('AWESOME_WEATHER_APPID', 'awesome_weather_appid');
+
+/** API for Yahoo Weather (for Awsome Weather Widget Pro)
+ * URL:https://developer.yahoo.com/apps/create
+ * Note: Yahoo weather appears to be broken
+ **/
+define( 'AWESOME_WEATHER_YAHOO_CONSUMER', 'yhoo_consumer_key' );
+define( 'AWESOME_WEATHER_YAHOO_SECRET', 'yhoo_secret_key' );
+
+/** API for Weather Underground (for Awsome Weather Widget Pro)
+ * URL: https://www.wunderground.com/weather/api
+ **/
+define('AWESOME_WUNDERGROUND_KEY', 'wundergroun_key');
+
+/** 
+ * License for Gravity Forms 
+ * URL: https://gravityforms.com
+ **/
+ define(‘GF_LICENSE_KEY’,'gravity_forms_license_key’); 
